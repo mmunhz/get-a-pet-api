0c762cd30680c89c4de5444c5ad9ffe1e266d284
mmunhz <mmunhz.ufabc@gmail.com>
2022-01-31 11:57:21 -0300

    Add User authentication process

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,6 +1,10 @@
 const User = require('../models/User')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+// helpers
 const createUserToken = require('../helpers/create-user-token')
+const getToken = require('../helpers/get-token')
 
 module.exports = class UserController {
 
@@ -104,6 +108,22 @@ module.exports = class UserController {
         }
 
         await createUserToken(user, req, res)
-        
+    }
+
+    static async checkUser(req, res) {
+
+        let currentUser
+
+        if(req.headers.authorization) {
+            const token = getToken(req)
+            const decoded = jwt.verify(token, 'secret')
+
+            currentUser = await User.findById(decoded.id)
+
+            currentUser.password = undefined
+        } else {
+            currentUser = null
+        }
+        res.status(200).send(currentUser)
     }
 }
