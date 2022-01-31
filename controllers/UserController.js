049c683152b12d6514d10db48a16c49a1d105994
mmunhz <mmunhz.ufabc@gmail.com>
2022-01-31 02:06:10 -0300

    Add login post route and validations

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -70,4 +70,40 @@ module.exports = class UserController {
         }
 
     }
+
+    static async login(req, res) {
+        const { email, password } = req.body
+
+        if (!email) {
+            res.status(422).json({ message: "O e-mail é obrigatório" })
+            return
+        }
+
+        if (!password) {
+            res.status(422).json({ message: "A senha é obrigatória" })
+            return
+        }
+
+        const user = await User.findOne({ email: email })
+        if (!user) {
+            res
+                .status(422)
+                .json({
+                    message: 'Não há usuário cadastrado com esse email',
+                })
+            return
+        }
+
+        // check if password match with db password
+        const checkPassword = await bcrypt.compare(password, user.password)
+
+        if(!checkPassword) {
+            res.status(422).json({
+                message: "Senha inválida!",
+            })
+        }
+
+        await createUserToken(user, req, res)
+        
+    }
 }
