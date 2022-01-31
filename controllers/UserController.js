4af8f7a0e0b81046f6b8537a7a8b3d9acd15307d
mmunhz <mmunhz.ufabc@gmail.com>
2022-01-31 19:50:53 -0300

    Add update user route

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -5,6 +5,7 @@ const jwt = require('jsonwebtoken')
 // helpers
 const createUserToken = require('../helpers/create-user-token')
 const getToken = require('../helpers/get-token')
+const getUserByToken = require('../helpers/get-user-by-tooken')
 
 module.exports = class UserController {
 
@@ -105,6 +106,7 @@ module.exports = class UserController {
             res.status(422).json({
                 message: "Senha inválida!",
             })
+            return
         }
 
         await createUserToken(user, req, res)
@@ -143,10 +145,89 @@ module.exports = class UserController {
     }
 
     static async editUser(req, res) {
-        res.status(200).json({
-            message: 'Deu certo update!'
-        })
-        return
+
+        const id = req.params.id
+
+        const { name, email, phone, password, confirmpassword } = req.body
+
+        let image = ''
+
+        // check if user exists
+        const token = getToken(req)
+        const user = await getUserByToken(token)
+
+        if (!user) {
+            res.status(422).json({
+                message: 'Usuário não encontrado!'
+            })
+            return
+        }
+
+        // validations
+        if (!name) {
+            res.status(422).json({ message: 'O nome é obrigatório' })
+            return
+        }
+
+        if (!email) {
+            res.status(422).json({ message: "O e-mail é obrigatório" })
+            return
+        }
+
+        // check if email has already been taken 
+        const userExists = await User.findOne({ email: email })
+
+        if (!userExists) {
+            res.status(422).json({
+                message: 'Usuário não encontrado!'
+            })
+            return
+        }
+
+        if (user.email !== email) {
+            res.status(422).json({
+                message: 'Por favor, utilize outro email!'
+            })
+            return
+        }
+        user.email = email
+
+        if (!phone) {
+            res.status(422).json({ message: "O telefone é obrigatório" })
+            return
+        }
+        user.phone = phone
+
+        if (password !== confirmpassword) {
+            res.status(422).json({ message: 'As senhas não conferem!' })
+            return
+        } else if (password === confirmpassword && password != null) {
+            // create password
+
+            const salt = await bcrypt.genSalt(12)
+            const passwordHash = await bcrypt.hash(password, salt)
+
+            user.password = passwordHash
+        }
+
+        try {
+            // return user updated data 
+            await User.findOneAndUpdate(
+                { _id: user._id },
+                { $set: user },
+                { new: true },
+            )
+
+            res.status(200).json({
+                message: "Usuário atualizado com sucesso"
+            })
+
+        } catch (error) {
+            res.status(500).json({ message: error })
+            return
+        }
+
+
     }
 
 }
