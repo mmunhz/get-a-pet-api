1ef3008f2d3e5095a2a2bfb69f1bad5cffdbbf6c
mmunhz <mmunhz.ufabc@gmail.com>
2022-02-01 13:53:08 -0300

    Add Pet details route

--- controllers/PetController.js
+++ controllers/PetController.js
@@ -3,7 +3,7 @@ const Pet = require("../models/Pet")
 // helpers
 const getToken = require("../helpers/get-token")
 const getUserByToken = require("../helpers/get-user-by-tooken")
-
+const ObjectId = require("mongoose").Types.ObjectId
 
 module.exports = class PetController {
 
@@ -28,7 +28,7 @@ module.exports = class PetController {
             res.status(422).json({ message: "A idade é obrigatória" })
             return
         }
-        
+
         if (!weight) {
             res.status(422).json({ message: "O peso é obrigatório" })
             return
@@ -94,12 +94,44 @@ module.exports = class PetController {
         const token = getToken(req)
         const user = await getUserByToken(token)
 
-        const pets = await Pet.find({'user._id': user._id}).sort('-createdAt')
+        const pets = await Pet.find({ 'user._id': user._id }).sort('-createdAt')
 
         res.status(200).json({
             pets,
         })
 
+    }
+
+    static async getAllUserAdoptions(req, res) {
+        // get user from token
+        const token = getToken(req)
+        const user = await getUserByToken(token)
+
+        const pets = await Pet.find({ 'adopter._id': user._id }).sort('-createdAt')
+
+        res.status(200).json({
+            pets,
+        })
+
+    }
+
+    static async getPetById(req, res) {
+        const id = req.params.id
+
+        if (!ObjectId.isValid(id)) {
+            res.status(422).json({ message: 'ID inválido!' })
+            return
+        }
+        // check if pet exists
+        const pet = await Pet.findOne({ _id: id })
+
+        if(!pet) {
+            res.status(404).json({ mesasge: 'Pet não encontrado!'})
+        }
+
+        res.status(200).json({
+            pet: pet,
+        })
     }
 
 }
